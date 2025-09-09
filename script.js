bfda94d7b57c209b1644d431b8acbd3d44b88e36
thalis02souza-maker <thalis02souza@gmail.com>
2025-09-09 15:04:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const mensagens = [
-"vocẽ e mais forte do que imagina 💛"
-"nunca se esqueça: sua vida importa 🌻"
-"vocẽ não ésta sozinho, estamos juntos 🤝🏿"
-"acredite:dias melhores virão ☀️"
-"sempre existe  esperança 🏳️‍🌈"
+  "Você é mais forte do que imagina 💛",
+  "Nunca se esqueça: sua vida importa 🌻",
+  "Você não está sozinho, estamos juntos 🤝🏿",
+  "Acredite: dias melhores virão ☀️",
+  "Sempre existe esperança 🏳️‍🌈"
 ];
 
-document.getElementById("btnMensagem").addEventListener("click", () =>{
-  const aleatoria = mensagem[Math.floor(Math.random() *  mensagem.length)];
+document.getElementById("btnMensagem").addEventListener("click", () => {
+  const aleatoria = mensagens[Math.floor(Math.random() * mensagens.length)];
   document.getElementById("mensagem").innerText = aleatoria;
 });
